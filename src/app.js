b8ba951d4a6d5430136e43b5dc6be51f0dbad68b
Ezequiel Blanco <ezequielgier19@gmail.com>
2024-11-29 20:13:35 +0000

    ejercicio completo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El Oso", "Mi vecino", "Un nudista", "El Narcotraficante"];
+
+  let action = ["comió", "cagó", "reventó", "destrozó"];
+
+  let what = ["mi código", "mi teléfono", "el coche"];
+
+  let when = [
+    "antes de clases",
+    "cuando estaba durmiendo",
+    "mientras estaba duchándome",
+    "durante mi cena",
+    "mientras veia el Barca"
+  ];
+  let random_number_who = Math.floor(Math.random() * who.length);
+  let random_number_action = Math.floor(Math.random() * action.length);
+  let random_number_what = Math.floor(Math.random() * what.length);
+  let random_number_when = Math.floor(Math.random() * when.length);
+  document.getElementById("excuse").innerHTML =
+    who[random_number_who] +
+    " " +
+    action[random_number_action] +
+    " " +
+    what[random_number_what] +
+    " " +
+    when[random_number_when];
 };
